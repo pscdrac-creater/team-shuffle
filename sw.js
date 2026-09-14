6a1c8924bbb3e07e5174581e7eb5b24df4af0386
pscdrac-creater <227791042+pscdrac-creater@users.noreply.github.com>
2026-09-14 14:30:25 +0530

    Deploy built site

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teamshuffle-v6';
+const CACHE = 'teamshuffle-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png'];
 
